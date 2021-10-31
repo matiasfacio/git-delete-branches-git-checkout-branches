34c10556d5977872da0eb17bebcbb449efacc055
Matias Facio <matiaspersonal@gmail.com>
2021-10-31 23:40:47 +0100

    handles multipe inputs

--- gdb.js
+++ gdb.js
@@ -3,7 +3,7 @@
 const { exec } = require("child_process");
 const inquirer = require("inquirer");
 
-exec("git branch", (error, stdout, stderr) => {
+exec("git branch", async (error, stdout, stderr) => {
   if (error) {
     console.error(error);
     return;
@@ -20,33 +20,52 @@ exec("git branch", (error, stdout, stderr) => {
     return value.replace(/(\n$)/gm, "");
   });
 
-  inquirer
-    .prompt([
+  const selectedByUser = await inquirer.prompt([
+    {
+      type: "checkbox",
+      name: "branches",
+      message: "which branches do you want to delete?",
+      choices: [...filteredNewBranch, new inquirer.Separator()],
+    },
+  ]);
+
+  if (selectedByUser.branches.length > 0) {
+    const message =
+      selectedByUser.branches.length === 1
+        ? `Are you sure that you want to delete this branch? [${selectedByUser.branches}]`
+        : `Are you sure that you want to delete these branches? [${selectedByUser.branches.join(
+            ", "
+          )}]`;
+
+    const confirm = await inquirer.prompt([
       {
-        type: "checkbox",
-        name: "branches",
-        message: "which branches do you want to delete?",
-        choices: filteredNewBranch,
+        type: "confirm",
+        name: "accepted",
+        message: message,
       },
-    ])
-    .then((answers) =>
-      exec(`git branch -D ${answers.branches}`, (error, stdout, stderr) => {
-        if (error) {
-          console.log("sorry, there was an error");
-        }
-        if (stderr) {
-          if (
-            answers.branches.includes("main") ||
-            answers.branches.includes("master")
-          ) {
-            console.error(`you cannot delete ${answers.branches}`);
-            return;
+    ]);
+
+    if (confirm.accepted) {
+      selectedByUser.branches.map((branch) => {
+        exec(`git branch -D ${branch}`, (error, stdout, stderr) => {
+          if (error) {
+            console.log("sorry, there was an error");
           }
-          console.error(stderr, " > are you currently on this branch ?");
-        }
-        if (stdout) {
-          console.log("Successful:", stdout);
-        }
-      })
-    );
+          if (stderr) {
+            if (
+              selectedByUser.branches.includes("main") ||
+              selectedByUser.branches.includes("master")
+            ) {
+              console.error(`you cannot delete ${selectedByUser.branches}`);
+              return;
+            }
+            console.error(stderr, " > are you currently on this branch ?");
+          }
+          if (stdout) {
+            console.log("Successful:", stdout);
+          }
+        });
+      });
+    }
+  }
 });
